34985b2ceffce030586b80885d8c1dd31a65f79d
prefrontalvortex <mikemcdermott23@gmail.com>
2017-03-02 23:02:58 -0500

    finished luna resampling, seems to work

--- downsample.py
+++ downsample.py
@@ -54,37 +54,46 @@ def get_pixels_hu(scans):
     return np.array(image, dtype=np.int16)
 
 #-----------------------------------------------------
-def resample_dcm(image, scan, new_spacing=[1, 1, 1]):
+def resample_dcm(image, scan, new_spacing=(1, 1, 1), verbose=False):
     '''Resampling to make x-y-z pixel spacing standardized'''
     
     # Determine current pixel spacing
     spacing = map(float, ([scan[0].SliceThickness] + scan[0].PixelSpacing))
     spacing = np.array(list(spacing))
+    if verbose: print('Initial Spacing:', spacing)
+
 
     resize_factor = spacing / new_spacing
     new_real_shape = image.shape * resize_factor
     new_shape = np.round(new_real_shape)
     real_resize_factor = new_shape / image.shape
     new_spacing = spacing / real_resize_factor
+    if verbose: print('Final Spacing:', new_spacing)
+
     
     image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
     
     return image, new_spacing
 
 
-def resample_mhd(image, new_spacing=[1, 1, 1]):
+def resample_mhd(rawScan, new_spacing=(1, 1, 1), verbose=False):
     '''Resampling to make x-y-z pixel spacing standardized'''
-
+    image = np.array(sitk.GetArrayFromImage(rawScan), dtype=np.int16)
+    spacing = np.array(rawScan.GetSpacing())  # spacing of voxels in world coor. (mm)
+    sx, sy, sz = spacing
+    spacing = np.array([sz, sx, sy])
     # Determine current pixel spacing
-    spacing = np.array(image.GetSpacing())  # spacing of voxels in world coor. (mm)
-
+    if verbose: print('Initial Spacing: {} Shape: {}'.format(spacing, image.shape))
     resize_factor = spacing / new_spacing
     new_real_shape = image.shape * resize_factor
     new_shape = np.round(new_real_shape)
     real_resize_factor = new_shape / image.shape
     new_spacing = spacing / real_resize_factor
 
+
     image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
+    if verbose: print('Final Spacing:   {} Shape: {}'.format(new_spacing, image.shape))
+
 
     return image, new_spacing
 
@@ -97,20 +106,21 @@ def load_mhd(filepath):
     return itk_img
 
 #-
-def file_to_array(filepath, newSpacing=(1, 1, 1)):
+def file_to_array(filepath, newSpacing=(1, 1, 1), verbose=False):
     files_dicom = glob.glob(filepath + '/*.dcm')
-    files_mhd = glob.glob(filepath + '/*.mhd')
+    # files_mhd = glob.glob(filepath + '/*.mhd')
     if files_dicom:
         rawDataAry = load_scan(filepath)
         patient_pixels = get_pixels_hu(rawDataAry)
-        pix_resampled, spacing = resample_dcm(patient_pixels, rawDataAry, newSpacing)
+        pix_resampled, spacing = resample_dcm(patient_pixels, rawDataAry, newSpacing, verbose=verbose)
     # return pix_resampled, spacing
     # print(filename.split('.'))
-    elif files_mhd:
+    elif '.mhd' in filepath:
         rawDataAry = load_mhd(filepath)
-        patient_pixels = sitk.GetArrayFromImage(rawDataAry)
+
+        # patient_pixels = np.swapaxes(patient_pixels, 2, 0)
         origin = np.array(rawDataAry.GetOrigin())  # x,y,z  Origin in world coordinates (mm)
-        pix_resampled, spacing = resample_mhd(patient_pixels, newSpacing)
+        pix_resampled, spacing = resample_mhd(rawDataAry, newSpacing, verbose=verbose)
     else:
         return None, None
     return pix_resampled, spacing
@@ -118,40 +128,43 @@ def file_to_array(filepath, newSpacing=(1, 1, 1)):
 
 
 #-----------------------------------------------------
-# wd = '/gpfs/scratch/delton17'
-wd = '/media/mike/tera/data/databowl/'
-input_folder = wd+'/kgsamples/' # '/CTscans/'
-resampled_folder = wd + '/resampled_images/'
-downsampled_folder = wd + '/downsampled_images/'
 
-if not os.path.exists(resampled_folder):
-    os.makedirs(resampled_folder)
-    
-if not os.path.exists(downsampled_folder):
-    os.makedirs(downsampled_folder)
+if __name__ == '__main__':
+    # wd = '/gpfs/scratch/delton17'
+    wd = '/media/mike/tera/data/databowl/'
+    # input_folder = wd+'/kgsamples/' # '/CTscans/'
+    input_folder = wd+'/luna/subset0/'
+    resampled_folder = wd + '/resampled_images/'
+    downsampled_folder = wd + '/downsampled_images/'
 
-patients = os.listdir(input_folder)
-patients.sort()
+    if not os.path.exists(resampled_folder):
+        os.makedirs(resampled_folder)
 
-t1 = time.time()
-print(patients)
-for i in tqdm(range(0,len(patients))):
-    # patient = load_scan(input_folder + patients[i])
-    # patient_pixels = get_pixels_hu(patient)
-    # pix_resampled, spacing = resample(patient_pixels, patient, [1,1,1])
-    pix_resampled, spacing = file_to_array(input_folder + patients[i])
-    if pix_resampled is not None:
-    
-        np.save(resampled_folder + patients[i], pix_resampled)
+    if not os.path.exists(downsampled_folder):
+        os.makedirs(downsampled_folder)
 
-        # ratio = 0.5
-        # downsampled = scipy.ndimage.interpolation.zoom(pix_resampled, [ratio, ratio, ratio])
-        # np.save(downsampled_folder + patients[i] + "_.5", downsampled)
-        #
-        #
-        # ratio = 0.25
-        # downsampled = scipy.ndimage.interpolation.zoom(pix_resampled, [ratio, ratio, ratio])
-        # np.save(downsampled_folder + patients[i] + "_.25", downsampled)
-    
-    # print("done with %1i" % i)
+    patients = os.listdir(input_folder)
+    patients.sort()
+
+    t1 = time.time()
+    print(patients)
+    for i in tqdm(range(0,len(patients))):
+        # patient = load_scan(input_folder + patients[i])
+        # patient_pixels = get_pixels_hu(patient)
+        # pix_resampled, spacing = resample(patient_pixels, patient, [1,1,1])
+        pix_resampled, spacing = file_to_array(input_folder + patients[i], verbose=True)
+        if pix_resampled is not None:
+
+            np.save(resampled_folder + patients[i], pix_resampled)
+
+            # ratio = 0.5
+            # downsampled = scipy.ndimage.interpolation.zoom(pix_resampled, [ratio, ratio, ratio])
+            # np.save(downsampled_folder + patients[i] + "_.5", downsampled)
+            #
+            #
+            # ratio = 0.25
+            # downsampled = scipy.ndimage.interpolation.zoom(pix_resampled, [ratio, ratio, ratio])
+            # np.save(downsampled_folder + patients[i] + "_.25", downsampled)
+
+        # print("done with %1i" % i)
 
