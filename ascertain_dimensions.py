f51259b5c5b14ba2c55e30d38bbd41c5183b45a5
prefrontalvortex <mikemcdermott23@gmail.com>
2017-03-02 19:14:34 -0500

    update

--- ascertain_dimensions.py
+++ ascertain_dimensions.py
@@ -1,6 +1,6 @@
 import numpy as np, pandas as pd
 import glob
-import os
+import os, sys
 from tqdm import tqdm
 
 # class Get_Npz_Shape():
@@ -18,8 +18,13 @@ from tqdm import tqdm
 #         if self.verbose:
 #             self._print_formatted()
 
+def get_npz_shape(filename):
+    myfile = np.load(resampled_folder + patients[i] + '.npy')
+    return list(np.shape(myfile))
+
 if __name__ == '__main__':
     # wd = '/gpfs/scratch/delton17'
+    # input_folder = wd + '/CTscans/'
     wd = '/media/mike/tera/data/databowl/'
     input_folder = wd + '/kgsamples/' # '/CTscans/'
     resampled_folder = wd + '/resampled_images/'
@@ -27,15 +32,22 @@ if __name__ == '__main__':
 
     patients = os.listdir(input_folder)
     patients.sort()
+    np.array()
 
     shapes = []
     for i in tqdm(range(0, len(patients))):
-        myfile = np.load(resampled_folder + patients[i] + '.npy')
-        shape = list(np.shape(myfile))
-        row = [patients[i], ] + shape
-        shapes.append(row)
-
-    shapes = pd.DataFrame(shapes, columns=['id','z', 'y', 'x'])
-    shapes.to_csv(wd + 'shapes.csv', index=False)
+        try:
+            shape = get_npz_shape(resampled_folder + patients[i] + '.npy')
+            row = [patients[i], ] + shape
+            shapes.append(row)
+        except KeyboardInterrupt:
+            sys.exit()
+        except Exception as exc:
+            print(exc)
+        if i % 5 == 0:
+            shapes_df = pd.DataFrame(shapes, columns=['id', 'z', 'y', 'x'])
+            shapes_df.to_csv(wd + '/' + 'shapes.csv', index=False)
+    shapes_df = pd.DataFrame(shapes, columns=['id','z', 'y', 'x'])
+    shapes_df.to_csv(wd + '/' + 'shapes.csv', index=False)
 
 
